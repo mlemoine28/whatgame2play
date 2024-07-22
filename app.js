7d5ae4f2ebcd7f2f805ad0e18e404cd9e12b3ca1
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-07-22 00:43:20 -0230

    Adjusted code - should be functioning correctly

--- app.js
+++ app.js
@@ -17,12 +17,11 @@ function App() {
             <Button
                 onClick={buttonClick}
                 label={"Click me! Now!"}
-            ></Button>
+            />
         </div>
     );
 }
 
 
 
- //not sure what the "default" part is though. However my understanding is that the export part is what makes it useable throughout the whole application in multiple facets.
-export default App;
+export default App; //not sure what the "default" part is though
