3a05e38e7683edacf46393796fd56e0073b51f1a
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-07-21 22:38:59 -0230

    add a person function, used destructuring

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 import React from "react";
 
-function App() {
-    return (
-        <>
-            <button>-</button>
-            <span>0</span>
-            <button>+</button>        
-            </>
-    )
-}  
+function Person({ name, age, colour}) {
+   return (
+    <div>
+        <h2>Name: {name}</h2>
+        <h2>Age: {age}</h2>
+        <h2>Colour: {colour}</h2>
+    </div> 
+   );
+}
+
+<Person 
+    name="Michael" 
+    age={34} 
+    colour="blue"
+/>
 
 export default App;
