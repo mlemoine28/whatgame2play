e5f72e21f32e9784b68188bdf495725bda4ea20a
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-07-22 00:28:25 -0230

    Added person.js, button.js, added more to app.js

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 import React from "react";
+import Person from "./person";
+import Button from "./button";
 
-function Person({ name, age, colour}) {
-   return (
-    <div>
-        <h2>Name: {name}</h2>
-        <h2>Age: {age}</h2>
-        <h2>Colour: {colour}</h2>
-    </div> 
-   );
+function App() {
+    const buttonClick = () => {
+        alert('Button clicked! Nice!');
+    };
+    
+    return (
+        <div>
+            <Person 
+                name="Michael" 
+                age={34} 
+                colour="blue"
+            />
+            <Button
+                onClick={buttonClick}
+                label={"Click me! Now!"}
+            ></Button>
+        </div>
+    );
 }
 
-<Person 
-    name="Michael" 
-    age={34} 
-    colour="blue"
-/>
 
+
+ //not sure what the "default" part is though. However my understanding is that the export part is what makes it useable throughout the whole application in multiple facets.
 export default App;
